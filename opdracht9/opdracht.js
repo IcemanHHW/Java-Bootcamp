a965a5754db02c9587bf2b8322eb1248ef8224b6
IcemanHHW <maartenkampmeijer@hotmail.com>
2017-09-18 11:39:51 +0200

    Add files via upload

---
+++ opdracht9/opdracht.js
@@ -0,0 +1,8 @@
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+document.getElementById("demo1").innerHTML = multiplyNumbers(21, 3);
+
+function sayHello(name) {
+  alert('Hello ' + name + '!');
+}
